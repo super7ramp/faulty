6fc90a8019c90bb7d87e7365f620729ac44ae23e
Antoine Belvire <antoine.belvire@opensuse.org>
2020-06-14 22:15:01 +0200

    Fix build with Java 8

--- faulty-api/src/main/java/io/github/super7ramp/faulty/api/Faulty.java
+++ faulty-api/src/main/java/io/github/super7ramp/faulty/api/Faulty.java
@@ -1,5 +1,7 @@
 package io.github.super7ramp.faulty.api;
 
+import java.util.Iterator;
+import java.util.Optional;
 import java.util.ServiceLoader;
 
 import io.github.super7ramp.faulty.spi.FaultyServicesProvider;
@@ -28,6 +30,21 @@ public final class Faulty {
 	 *                                found
 	 */
 	public static final FaultyServices getServices() throws AgentNotFoundException {
-		return LOADER.findFirst().map(FaultyServicesProvider::getServices).orElseThrow(AgentNotFoundException::new);
+		return findFirst().map(FaultyServicesProvider::getServices).orElseThrow(AgentNotFoundException::new);
+	}
+
+	/**
+	 * Find first implementation of {@link FaultyServicesProvider}.
+	 * 
+	 * @return The first service provider or empty {@code Optional} if no service
+	 *         providers are located
+	 */
+	private static Optional<FaultyServicesProvider> findFirst() {
+		final Iterator<FaultyServicesProvider> serviceProviders = LOADER.iterator();
+		if (serviceProviders.hasNext()) {
+			return Optional.of(serviceProviders.next());
+		} else {
+			return Optional.empty();
+		}
 	}
 }
